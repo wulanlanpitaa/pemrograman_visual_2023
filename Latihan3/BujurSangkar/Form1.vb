3ed9efb3060a1c798b5c7ca96782d5d671734870
wulanlanpitaa <103301047+wulanlanpitaa@users.noreply.github.com>
2023-04-05 09:49:31 +0700

    Delete Form1.vb

--- Latihan3/BujurSangkar/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles luasbs.Click
-        Thasil.Text = Val(Tpanjang.Text) * Val(Tpanjang.Text)
-    End Sub
-
-    Private Sub kelilingbs_Click(sender As Object, e As EventArgs) Handles kelilingbs.Click
-        Thasil.Text = 4 * Val(Tpanjang.Text)
-    End Sub
-End Class
