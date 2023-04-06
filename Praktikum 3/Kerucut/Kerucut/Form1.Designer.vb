8e9739afa956395cc31266428537c120f378c5bf
wulanlanpitaa <103301047+wulanlanpitaa@users.noreply.github.com>
2023-04-06 09:12:04 +0700

    Add files via upload

---
+++ Praktikum 3/Kerucut/Kerucut/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Tjarijari = New System.Windows.Forms.TextBox()
+        Me.Tsisi = New System.Windows.Forms.TextBox()
+        Me.Ttinggi = New System.Windows.Forms.TextBox()
+        Me.Volumekerucut = New System.Windows.Forms.Button()
+        Me.Luaspermukaankerucut = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Thasil = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(235, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Kerucut"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(80, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jari-Jari"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(82, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Sisi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(82, 278)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tinggi"
+        '
+        'Tjarijari
+        '
+        Me.Tjarijari.Location = New System.Drawing.Point(303, 101)
+        Me.Tjarijari.Name = "Tjarijari"
+        Me.Tjarijari.Size = New System.Drawing.Size(100, 22)
+        Me.Tjarijari.TabIndex = 4
+        '
+        'Tsisi
+        '
+        Me.Tsisi.Location = New System.Drawing.Point(303, 199)
+        Me.Tsisi.Name = "Tsisi"
+        Me.Tsisi.Size = New System.Drawing.Size(100, 22)
+        Me.Tsisi.TabIndex = 5
+        '
+        'Ttinggi
+        '
+        Me.Ttinggi.Location = New System.Drawing.Point(303, 281)
+        Me.Ttinggi.Name = "Ttinggi"
+        Me.Ttinggi.Size = New System.Drawing.Size(100, 22)
+        Me.Ttinggi.TabIndex = 6
+        '
+        'Volumekerucut
+        '
+        Me.Volumekerucut.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Volumekerucut.Location = New System.Drawing.Point(117, 374)
+        Me.Volumekerucut.Name = "Volumekerucut"
+        Me.Volumekerucut.Size = New System.Drawing.Size(192, 41)
+        Me.Volumekerucut.TabIndex = 7
+        Me.Volumekerucut.Text = "Volumekerucut"
+        Me.Volumekerucut.UseVisualStyleBackColor = True
+        '
+        'Luaspermukaankerucut
+        '
+        Me.Luaspermukaankerucut.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Luaspermukaankerucut.Location = New System.Drawing.Point(398, 374)
+        Me.Luaspermukaankerucut.Name = "Luaspermukaankerucut"
+        Me.Luaspermukaankerucut.Size = New System.Drawing.Size(313, 53)
+        Me.Luaspermukaankerucut.TabIndex = 8
+        Me.Luaspermukaankerucut.Text = "Luaspermukaankerucut"
+        Me.Luaspermukaankerucut.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(90, 509)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 25)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Hasil"
+        '
+        'Thasil
+        '
+        Me.Thasil.Location = New System.Drawing.Point(314, 519)
+        Me.Thasil.Name = "Thasil"
+        Me.Thasil.Size = New System.Drawing.Size(100, 22)
+        Me.Thasil.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(842, 604)
+        Me.Controls.Add(Me.Thasil)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Luaspermukaankerucut)
+        Me.Controls.Add(Me.Volumekerucut)
+        Me.Controls.Add(Me.Ttinggi)
+        Me.Controls.Add(Me.Tsisi)
+        Me.Controls.Add(Me.Tjarijari)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Perhitungan Kerucut Wulan Puspita"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Tjarijari As TextBox
+    Friend WithEvents Tsisi As TextBox
+    Friend WithEvents Ttinggi As TextBox
+    Friend WithEvents Volumekerucut As Button
+    Friend WithEvents Luaspermukaankerucut As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Thasil As TextBox
+End Class
---
+++ Praktikum 3/Kerucut/Kerucut/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Volumekerucut_Click(sender As Object, e As EventArgs) Handles Volumekerucut.Click
+        Dim jarijari, sisi, tinggi, hasil As Double
+        jarijari = Val(Tjarijari.Text)
+        sisi = Val(Tsisi.Text)
+        tinggi = Val(Ttinggi.Text)
+        hasil = Val(Thasil.Text)
+
+        Thasil.Text = 1 / 3 * 22 / 7 * Val(Tjarijari.Text) ^ 2 * Val(Ttinggi.Text)
+    End Sub
+
+    Private Sub Luaspermukaankerucut_Click(sender As Object, e As EventArgs) Handles Luaspermukaankerucut.Click
+        Thasil.Text = 22 / 7 * Val(Tjarijari.Text) * Val(Tjarijari.Text) + Val(Tsisi.Text)
+    End Sub
+End Class
