060ab370fa1d475245486df808de9815505a6f75
wulanlanpitaa <103301047+wulanlanpitaa@users.noreply.github.com>
2023-04-07 10:19:57 +0700

    Add files via upload

---
+++ Latihan 4/Celcius/Celcius/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.txtReamur = New System.Windows.Forms.TextBox()
+        Me.txtKelvin = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(50, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Celcius"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(50, 215)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fahrenheit"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(50, 294)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Reamur"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(50, 377)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Kelvin"
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Location = New System.Drawing.Point(264, 42)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(100, 22)
+        Me.txtCelcius.TabIndex = 4
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Location = New System.Drawing.Point(264, 219)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(100, 22)
+        Me.txtFahrenheit.TabIndex = 5
+        '
+        'txtReamur
+        '
+        Me.txtReamur.Location = New System.Drawing.Point(264, 298)
+        Me.txtReamur.Name = "txtReamur"
+        Me.txtReamur.Size = New System.Drawing.Size(100, 22)
+        Me.txtReamur.TabIndex = 6
+        '
+        'txtKelvin
+        '
+        Me.txtKelvin.Location = New System.Drawing.Point(264, 377)
+        Me.txtKelvin.Name = "txtKelvin"
+        Me.txtKelvin.Size = New System.Drawing.Size(100, 22)
+        Me.txtKelvin.TabIndex = 7
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(244, 121)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(141, 42)
+        Me.btnConvert.TabIndex = 8
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 514)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtKelvin)
+        Me.Controls.Add(Me.txtReamur)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Konversi dari C ke F,R,K Wulan Puspita"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents txtReamur As TextBox
+    Friend WithEvents txtKelvin As TextBox
+    Friend WithEvents btnConvert As Button
+End Class
