3e6b1e9ec9a73b327e716670f4afc5dbff894fb7
wulanlanpitaa <103301047+wulanlanpitaa@users.noreply.github.com>
2023-05-11 16:19:15 +0700

    Add files via upload

---
+++ Latihan 6/Combo box/Combo box/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbprodi = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtbiaya = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnsubmit = New System.Windows.Forms.Button()
+        Me.txtbayar = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txttagihan = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(227, 219)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pilih Prodi"
+        '
+        'cmbprodi
+        '
+        Me.cmbprodi.FormattingEnabled = True
+        Me.cmbprodi.Location = New System.Drawing.Point(374, 215)
+        Me.cmbprodi.Name = "cmbprodi"
+        Me.cmbprodi.Size = New System.Drawing.Size(261, 24)
+        Me.cmbprodi.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(227, 265)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Biaya Registrasi"
+        '
+        'txtbiaya
+        '
+        Me.txtbiaya.Location = New System.Drawing.Point(374, 262)
+        Me.txtbiaya.Name = "txtbiaya"
+        Me.txtbiaya.Size = New System.Drawing.Size(244, 22)
+        Me.txtbiaya.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(227, 174)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Nama"
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(374, 174)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(244, 22)
+        Me.txtnama.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(348, 81)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(161, 29)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Info Registrasi"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Comic Sans MS", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(300, 115)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(255, 25)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Universitas Jamblang Bersatu"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(359, 453)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(276, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Wulan Puspita - 200511008 -TI20D"
+        '
+        'btnsubmit
+        '
+        Me.btnsubmit.Location = New System.Drawing.Point(440, 343)
+        Me.btnsubmit.Name = "btnsubmit"
+        Me.btnsubmit.Size = New System.Drawing.Size(124, 32)
+        Me.btnsubmit.TabIndex = 9
+        Me.btnsubmit.Text = "Submit"
+        Me.btnsubmit.UseVisualStyleBackColor = True
+        '
+        'txtbayar
+        '
+        Me.txtbayar.Location = New System.Drawing.Point(374, 304)
+        Me.txtbayar.Name = "txtbayar"
+        Me.txtbayar.Size = New System.Drawing.Size(244, 22)
+        Me.txtbayar.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(227, 304)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 17)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Pembayaran"
+        '
+        'txttagihan
+        '
+        Me.txttagihan.Location = New System.Drawing.Point(374, 391)
+        Me.txttagihan.Name = "txttagihan"
+        Me.txttagihan.Size = New System.Drawing.Size(244, 22)
+        Me.txttagihan.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(227, 391)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(103, 17)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Sisa Tagihan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ClientSize = New System.Drawing.Size(837, 523)
+        Me.Controls.Add(Me.txttagihan)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtbayar)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnsubmit)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtbiaya)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmbprodi)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Combo box - Wulan Puspita"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbprodi As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtbiaya As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnsubmit As Button
+    Friend WithEvents txtbayar As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txttagihan As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Latihan 6/Combo box/Combo box/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbprodi.Items.Clear()
+        cmbprodi.Items.Add("Manajemen")
+        cmbprodi.Items.Add("Teknik Informatika")
+        cmbprodi.Items.Add("Ilmu Komunikasi")
+        cmbprodi.Items.Add("PGSD")
+        cmbprodi.Items.Add("Hukum")
+        cmbprodi.Items.Add("Gizi")
+    End Sub
+
+    Private Sub cmbprodi_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbprodi.SelectedIndexChanged
+        If cmbprodi.Text = "Manajemen" Then
+            txtbiaya.Text = "9000000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        ElseIf cmbprodi.Text = "Teknik Informatika" Then
+            txtbiaya.Text = "9500000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        ElseIf cmbprodi.Text = "Ilmu Komunikasi" Then
+            txtbiaya.Text = "9400000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        ElseIf cmbprodi.Text = "PGSD" Then
+            txtbiaya.Text = "12000000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        ElseIf cmbprodi.Text = "Hukum" Then
+            txtbiaya.Text = "9600000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        ElseIf cmbprodi.Text = "Gizi" Then
+            txtbiaya.Text = "12500000"
+            txtbayar.Clear()
+            txttagihan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnsubmit_Click(sender As Object, e As EventArgs) Handles btnsubmit.Click
+        txttagihan.Text = Val(txtbiaya.Text) - Val(txtbayar.Text)
+    End Sub
+End Class
