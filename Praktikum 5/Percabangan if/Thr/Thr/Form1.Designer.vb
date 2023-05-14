9fa8505330453fff8b4eb56b090b5fb5470e1ea6
wulanlanpitaa <103301047+wulanlanpitaa@users.noreply.github.com>
2023-05-14 16:11:24 +0700

    Add files via upload

---
+++ Praktikum 5/Percabangan if/Thr/Thr/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMasaKerja = New System.Windows.Forms.TextBox()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.txtSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Masa Kerja"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(5, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Keterangan"
+        '
+        'txtMasaKerja
+        '
+        Me.txtMasaKerja.Location = New System.Drawing.Point(12, 89)
+        Me.txtMasaKerja.Name = "txtMasaKerja"
+        Me.txtMasaKerja.Size = New System.Drawing.Size(214, 22)
+        Me.txtMasaKerja.TabIndex = 2
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Location = New System.Drawing.Point(12, 223)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(363, 22)
+        Me.txtKeterangan.TabIndex = 3
+        '
+        'txtSubmit
+        '
+        Me.txtSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSubmit.Location = New System.Drawing.Point(326, 89)
+        Me.txtSubmit.Name = "txtSubmit"
+        Me.txtSubmit.Size = New System.Drawing.Size(126, 40)
+        Me.txtSubmit.TabIndex = 4
+        Me.txtSubmit.Text = "Submit"
+        Me.txtSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 371)
+        Me.Controls.Add(Me.txtSubmit)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.txtMasaKerja)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Tunjangan Hari Raya_Wulan Puspita"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMasaKerja As TextBox
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents txtSubmit As Button
+End Class
---
+++ Praktikum 5/Percabangan if/Thr/Thr/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub txtSubmit_Click(sender As Object, e As EventArgs) Handles txtSubmit.Click
+        If (Val(txtMasaKerja.Text) >= 30) Then
+            txtKeterangan.Text = "Selamat anda mendapatkan THR 1juta dan parcel"
+
+        ElseIf (txtMasaKerja.Text >= 15) Then
+            txtKeterangan.Text = "Selamat anda mendapatkan THR 700rb dan parcel"
+
+        ElseIf (txtMasaKerja.Text >= 10) Then
+            txtKeterangan.Text = "Selamat anda mendapatkan THR 500rb"
+
+        ElseIf (txtMasaKerja.Text >= 5) Then
+            txtKeterangan.Text = "Selamat anda mendapatkan THR 300rb"
+
+        Else
+            txtKeterangan.Text = "Maaf anda belum layak dapat THR"
+        End If
+    End Sub
+End Class
